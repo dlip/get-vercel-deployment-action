845f959c221d4a44d5443a89e8a7108c640346c6
Dane Lipscombe <danelipscombe@gmail.com>
2022-11-06 09:32:46 +1100

    Read inputs

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("hello world");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = __importDefault(require("@actions/core"));
+try {
+    const nameToGreet = core_1.default.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core_1.default.setOutput("time", time);
+}
+catch (error) {
+    core_1.default.setFailed(error.message);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world")
+import core from '@actions/core';
+
+try {
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+} catch (error: unknown) {
+  core.setFailed((error as Error).message);
+}
