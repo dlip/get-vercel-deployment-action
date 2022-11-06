17fb02a1313505c99f96771766626caecf271ed4
Dane Lipscombe <danelipscombe@gmail.com>
2022-11-06 13:59:33 +1100

    Fix quotes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,19 @@
-import core from '@actions/core';
+import core from "@actions/core";
 
 try {
-  const nameToGreet = core.getInput('who-to-greet');
-  const vercel-token = core.getInput('vercel-token');
-  const vercel-org-id = core.getInput('vercel-org-id');
-  const vercel-project-id = core.getInput('vercel-project-id');
-  const github-branch = core.getInput('github-branch');
-  const github-commit = core.getInput('github-commit');
-  const start-timeout = core.getInput('start-timeout');
-  const finish-timeout = core.getInput('finish-timeout');
-  const wait = core.getInput('wait');
-  const vercel-token = console.log({vercel-token});
-  const vercel-org-id = console.log({vercel-org-id});
-  const vercel-project-id = console.log({vercel-project-id});
-  const github-branch = console.log({github-branch});
-  const github-commit = console.log({github-commit});
-  const start-timeout = console.log({start-timeout});
-  const finish-timeout = console.log({finish-timeout});
-  const wait = console.log({wait});
-  core.setOutput("deployment-url", "TODO");
+  const args = {
+    vercelToken: core.getInput("vercel-token"),
+    vercelOrgId: core.getInput("vercel-org-id"),
+    vercelProjectId: core.getInput("vercel-project-id"),
+    githubBranch: core.getInput("github-branch"),
+    githubHash: core.getInput("github-hash"),
+    startTimeout: core.getInput("start-timeout"),
+    finishTimeout: core.getInput("finish-timeout"),
+    wait: core.getInput("wait"),
+  };
+  console.log(args);
+
+  core.setOutput("deployment-url", "todo");
 } catch (error: unknown) {
   core.setFailed((error as Error).message);
 }
