ca5664fc74666269bf90be381acd205544c11538
Dane Lipscombe <danelipscombe@gmail.com>
2022-11-06 14:23:08 +1100

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,46 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(require("@actions/core"));
+const core = __importStar(require("@actions/core"));
 try {
-    const nameToGreet = core_1.default.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core_1.default.setOutput("time", time);
+    console.log('one');
+    const args = {
+        vercelToken: core.getInput("vercel-token"),
+        vercelOrgId: core.getInput("vercel-org-id"),
+        vercelProjectId: core.getInput("vercel-project-id"),
+        githubBranch: core.getInput("github-branch"),
+        githubHash: core.getInput("github-hash"),
+        startTimeout: core.getInput("start-timeout"),
+        finishTimeout: core.getInput("finish-timeout"),
+        wait: core.getInput("wait"),
+    };
+    console.log('two');
+    console.log(args);
+    core.setOutput("deployment-url", "todo");
 }
 catch (error) {
-    core_1.default.setFailed(error.message);
+    console.error(error);
+    core.setFailed(error.message);
 }
